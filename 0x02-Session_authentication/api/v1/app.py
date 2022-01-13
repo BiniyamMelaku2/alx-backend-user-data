985551365f8cea3b038f5c9f0e511f16534eb33d
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-01-14 01:33:25 +0300

    flask app module

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,9 +64,10 @@ def before_request() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    current_user = auth.current_user(request)
     if current_user is None:
         abort(403)
-    request.current_user = auth.current_user(request)
+    request.current_user = current_user
 
 
 if __name__ == "__main__":
