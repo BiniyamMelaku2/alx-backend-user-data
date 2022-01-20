b778a54b7f2ae5f53a54c247929506fb966dd629
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-01-21 01:28:55 +0300

    authentication module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,7 +55,7 @@ class Auth:
             user.session_id = _generate_uuid()
             return user.session_id
 
-    def get_user_from_session_id() -> str:
+    def get_user_from_session_id(self, session_id: str) -> str:
         '''takes a single session_id & return the corresponding user.'''
         try:
             user = self._db.find_user_by(session_id=session_id)
@@ -73,3 +73,22 @@ class Auth:
         except NoResultFound:
             return None
         user.session_id = None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''update the user’s reset_token database field'''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        user.reset_token = _generate_uuid()
+        return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''hash the password and update the user’s hashed_password'''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password=password)
+            user.reset_token = None
