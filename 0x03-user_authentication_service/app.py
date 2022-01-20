41b97663de32916eb14ec6a0eaa0a861a7907eb7
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-01-21 00:47:48 +0300

    flaskapp module

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,6 +14,7 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'])
 def users() -> str:
     try:
@@ -26,6 +27,50 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    '''respond to the POST create a new session for the user'''
+    form_data = request.form
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if AUTH.valid_login(email, password) is False:
+            abort(401)
+        else:
+            session_id = AUTH.create_session(email)
+            response = jsonify({
+                "email": email,
+                "message": "logged in"})
+            response.set_cookie('session_id', session_id)
+            return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def log_out() -> None:
+    '''user with the requested session ID get the session destroyed'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    '''find the user respond with a 200 HTTP status'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
