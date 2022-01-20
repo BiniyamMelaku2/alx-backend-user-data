7cfa75ff278731ef5798097c0c9da0d00aa6e1d7
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-01-21 01:30:00 +0300

    python flask app module

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,35 @@ def profile() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def update_password() -> str:
+    '''respond to the PUT /reset_password to update password'''
+    try:
+        email = request.form["email"]
+    except KeyError:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    '''respond to the PUT /reset_password to update password'''
+    try:
+        email = request.form["email"]
+        reset_token = request.form["reset_token"]
+        new_password = request.form["new_password"]
+    except KeyError:
+        abort(400)
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
